f0eb5eb41715fc91714102761e0c8d794664db5e
Nicholas Wang <nicholaswang@Nicholass-MacBook-Pro.local>
2026-08-22 18:04:06 -0400

    Refactor codebase to be more organized, readable, and modular
    API functions no longer handle streamlit logic. Separates streamlit display into functions. Rename main.py to streamlit_app.py. Update README.md. Adds .gitignore and requirements.txt

--- API.py
+++ API.py
@@ -11,15 +11,12 @@ from the Finnhub API.
 import os
 import requests
 from dotenv import load_dotenv
-import streamlit as st
-import pandas as pd
 load_dotenv()
 API_KEY = os.getenv("STOCK_API_KEY")
 
 def get_stock_quote(symbol):
     """
     Retrieves the current stock quote for a given ticker symbol,
-    created a bar chart of the data as well as a table of the open/high/low/current/prev close
 
     Parameters:
         symbol (str): Stock ticker symbol.
@@ -28,23 +25,12 @@ def get_stock_quote(symbol):
     params = {"symbol": symbol, "token": API_KEY}
     response = requests.get(url, params=params)
     data = response.json()
-    
-    col1, col2 = st.columns(2)
-    chart_data = pd.DataFrame({
-    "Price": [data['o'], data['h'], data['l'], data['c'], data['pc']]
-    }, index=["Open", "High", "Low", "Current", "Prev Close"])
 
-    with col1:
-        st.subheader(f"{symbol}: Stock Data for Today")
-        st.bar_chart(chart_data)
-    with col2:
-        st.subheader(f"{symbol}: Stock Data Table")
-        st.dataframe(chart_data)
-    return data["pc"], data["c"] #to calculate returns
+    return data
 
 def get_company_news(symbol):
     """
-    Retrieves link to latest Company news data from Finnhub and displays as a selector button with retreived image.
+    Retrieves link to latest Company news data from Finnhub.
 
     Parameters:
             symbol (str): Stock ticker symbol.
@@ -54,22 +40,4 @@ def get_company_news(symbol):
     response = requests.get(url, params=params)
     data = response.json()
 
-    col1, col2, col3 = st.columns(3)
-    with col1:
-        st.image(
-            data[0]['image'],
-            use_container_width=True
-            )
-        st.link_button(data[0]['headline'], data[0]['url'])
-    with col2:
-        st.image(
-            data[1]['image'],
-            use_container_width=True
-            )
-        st.link_button(data[1]['headline'], data[1]['url'])
-    with col3:
-        st.image(
-            data[2]['image'],
-            use_container_width=True
-            )
-        st.link_button(data[2]['headline'], data[2]['url'])
+    return data
--- README.md
+++ README.md
@@ -10,13 +10,19 @@ GitHub repo: "aipi-503-python-group-2-final"
 git clone https://github.com/gitnic-duke/aipi-503-python-group-2-final.git
 cd aipi-503-python-group-2-final
 python -m venv .venv
-.venv/Scripts/activate.ps1
-pip install streamlit
-pip install dotenv
-pip install pandas
+.venv/Scripts/activate.ps1 # Windows or
+source .venv/bin/activate # Mac
+pip install -r requirements.txt
 
 ### Usage
-streamlit run main.py
+```
+### API Key set up
+1. Copy your API key from Finnhub.
+If you don't have one, create one on https://finnhub.io/
+2. Create a .streamlit/secrets.toml file with the content OPENWEATHER_API_KEY="{your_API_key}"
+3. Save the file
+```bash
+streamlit run streamlit_app.py
 
 ### Documentation
 .env #Stock API
